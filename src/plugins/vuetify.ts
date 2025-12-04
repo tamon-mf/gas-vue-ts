4dbf718dca6d3fe0b1478c548a4fe7b029b39076
tamon-mf <okamoto.tamon@moneyforward.co.jp>
2025-12-04 14:09:14 +0900

    Refactor Vuetify plugin by removing theme configuration and deleting unused theme file.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,19 +1,8 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vue from "vue";
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
 
-const opts = {
-  theme: {
-    themes: {
-      light: {
-        primary: '#3f51b5',
-        secondary: '#b0bec5',
-        accent: '#8c9eff',
-        error: '#b71c1c',
-      },
-    },
-  },
-};
+const opts = {};
 
 export default new Vuetify(opts);
